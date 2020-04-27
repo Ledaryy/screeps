e1133f268bb4c4bdd08246e8f554e3e79e9da7e1
iLedary <andrejkulishov@gmail.com>
2020-04-27 15:17:38 +0100

    New addon, for bridging in W2N6

--- role.bridge.js
+++ role.bridge.js
@@ -4,7 +4,8 @@ module.exports = {
 
 
         var creepCarryStorage = [];
-
+        var creepHome = creep.memory.home;
+        var creepPosition = creep.room.name;
 
         if (creep.carry.energy === creep.carryCapacity) {
             creepCarryStorage = 'full';
@@ -14,28 +15,77 @@ module.exports = {
             creepCarryStorage = 'not full'
         }
 
-        var container = fParsingModule(STRUCTURE_CONTAINER, RESOURCE_ENERGY, "more", 500);
+        var container = fParsingModule(STRUCTURE_CONTAINER, RESOURCE_ENERGY, "more", 800);
         var storage = fParsingModule(STRUCTURE_STORAGE, RESOURCE_ENERGY, "less", 1000000);
         var link = fParsingForLink("W1N6", 21, 17, 0);
         var droppedResource = creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES);
+        var mainContainer = fParsingForContainer('W2N6', 24, 27)
 
 
-        switch (true) {
-            case creepCarryStorage === "full":
-                fTransferEnergy(storage);
-                break;
-            case (creepCarryStorage === "empty" || creepCarryStorage === "not full") && link !== 0:
-                fWithdrawEnergy(link)
-                break;
-            case creepCarryStorage === "not full" && link === 0:
-                fTransferEnergy(storage);
-                break;
-            default:
-                creep.moveTo(21, 16);
-                break;
+switch (true) {
+    case creepPosition === creepHome:
+        if (creepHome === 'W1N6') {
+
+            switch (true) {
+                case creepCarryStorage === "full":
+                    fTransferEnergy(storage);
+                    break;
+                case (creepCarryStorage === "empty" || creepCarryStorage === "not full") && link !== 0:
+                    fWithdrawEnergy(link)
+                    break;
+                case creepCarryStorage === "not full" && link === 0:
+                    fTransferEnergy(storage);
+                    break;
+                default:
+                    creep.moveTo(21, 16);
+                    break;
+
+            }
+        }
+
+        if (creepHome === 'W2N6') {
+
+            switch (true) {
+                case creepCarryStorage === "full":
+                    fTransferEnergy(mainContainer);
+                    break;
+                case (creepCarryStorage === "empty" || creepCarryStorage === "not full"):
+                    fWithdrawEnergy(container)
+                    break;
+
+
+                default:
+                    creep.moveTo(24, 28);
+                    break;
+
+            }
+        }
+        break;
+
+    default:
+        fMoveToExit(creepHome)
+        break;
+
+}
+
+
+        function fMoveToExit(fTarget) {
+
+            let exit = creep.room.findExitTo(fTarget);
+            let exitPos = creep.pos.findClosestByRange(exit);
+
+            creep.moveTo(exitPos);
+
+
 
         }
 
+        function fParsingForContainer(roomName, xPosition, yPosition) {
+            let cont = Game.rooms[roomName].lookForAt('structure', xPosition, yPosition)[0];
+                return cont
+
+
+        }
 
         function fParsingForLink(roomName, xPosition, yPosition, neededValue) {
              var link = Game.rooms[roomName].lookForAt('structure', xPosition, yPosition)[0];
@@ -135,7 +185,7 @@ module.exports = {
             var withdrawing = creep.withdraw(fStorage, RESOURCE_ENERGY);
             switch (withdrawing) {
                 case ERR_NOT_IN_RANGE:
-                    creep.moveTo(fStorage);
+                    creep.moveTo(fStorage, {visualizePathStyle: {stroke: '#fffcf3'}});
                     break;
                 default:
 
