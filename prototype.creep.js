faa1fc36d4a6dd8fdbe1509812668c726a8a55ed
Ledary <andrejkulishov@gmail.com>
2020-05-19 21:37:22 +0100

    New functions: dataPack, creepDataPack

--- prototype.creep.js
+++ prototype.creep.js
@@ -1,3 +1,45 @@
+Creep.prototype.dataPack =
+    function () {
+        return {
+            spawn: this.findStructure(STRUCTURE_SPAWN, RESOURCE_ENERGY, "less", 300),
+            extension:  this.findStructure(STRUCTURE_EXTENSION, RESOURCE_ENERGY, "less", 100),
+            extensionLowPower:  this.findStructure(STRUCTURE_EXTENSION, RESOURCE_ENERGY, "less", 50),
+            tower:  this.findStructure(STRUCTURE_TOWER, RESOURCE_ENERGY, "less", 600),
+            storage:  this.findStructure(STRUCTURE_STORAGE, RESOURCE_ENERGY, "less", 1000001),
+            droppedResource:  this.pos.findClosestByRange(FIND_DROPPED_RESOURCES),
+            terminal: this.findStructure(STRUCTURE_TERMINAL, RESOURCE_ENERGY, "less", 300000),
+            linkW1N6: this.linkFinder("W1N6", 21, 17, 1),
+            linkW1N7: this.linkFinder('W1N7', 24, 23, 1),
+            linkW2N6: this.linkFinder("W2N6", 25, 29, 1)
+
+        }
+
+    }
+
+Creep.prototype.creepDataPack =
+    function () {
+        return {
+            creepHome: this.memory.home,
+            creepPosition: this.room.name,
+            creepEnergy: this.store.energy,
+            creepCapacity: this.store.getFreeCapacity(),
+            carryStorage: function (creepEnergy, creepCapacity) {
+
+                if (creepEnergy === creepCapacity){
+                    return 'full'
+                }
+                if (creepEnergy === 0){
+                    return 'empty'
+                }
+            }
+
+
+
+        }
+    }
+
+
+
 Creep.prototype.findStructure =
     function (structureType, energyType, math, neededValue ) { // math can be "more" "less" "equal"
     var foundedStructures = this.room.find(FIND_STRUCTURES, {
@@ -102,7 +144,7 @@ Creep.prototype.transferTo =
 
 Creep.prototype.withdrawFrom =
     function (fStorage, fResourceType) {
-        let withdrawing = this.withdraw(fStorage, RESOURCE_ENERGY);
+        let withdrawing = this.withdraw(fStorage, fResourceType);
         switch (withdrawing) {
             case ERR_NOT_IN_RANGE:
                 this.moveTo(fStorage);
@@ -113,3 +155,97 @@ Creep.prototype.withdrawFrom =
         }
     }
 
+Creep.prototype.harvestSource =
+    function () {
+    var source = this.pos.findClosestByPath(FIND_SOURCES);
+    var harvestSource = this.harvest(source);
+    switch (harvestSource) {
+        case 0:
+
+            break;
+        case ERR_NOT_IN_RANGE:
+            this.moveByRole();
+            break;
+        default:
+            this.say("🚬");
+            break;
+
+    }
+}
+
+Creep.prototype.moveByRole =
+    function () {
+    let room = this.memory.home;
+    let position = this.memory.position;
+
+    switch (true) {
+        case room === 'W1N6' && position === 'first':
+            this.moveTo(44,27); // 44,27
+            break;
+
+        case room === 'W1N6' && position === 'second':
+            this.moveTo(39,39);
+            break;
+
+        case room === 'W2N6' && position === 'first':
+            this.moveTo(45,29);
+            break;
+
+        case room === 'W2N6' && position === 'second':
+            this.moveTo(5,13);
+            break;
+
+        case room === 'W1N7' && position === 'first':
+            this.moveTo(25,10);
+            break;
+
+        case room === 'W1N7' && position === 'second':
+            this.moveTo(34,34);
+            break;
+
+
+
+        default:
+            break;
+    }
+
+
+}
+
+Creep.prototype.transferToLink =
+    function () {
+    var link = this.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: s => s.structureType === STRUCTURE_LINK
+    });
+    var moveEnergyToLink = this.transfer(link, RESOURCE_ENERGY);
+    switch (moveEnergyToLink) {
+        case 0:
+
+            break;
+        case ERR_NOT_IN_RANGE:
+            this.moveTo(link);
+            break;
+        case ERR_INVALID_TARGET:
+
+            break;
+        case ERR_FULL:
+
+            break;
+        default:
+
+            console.log("Err in fMoveEnergyToLink in harvest module!");
+            break;
+    }
+}
+
+Creep.prototype.linkFinder =
+    function (roomName, xPosition, yPosition, neededValue) {
+        var link = Game.rooms[roomName].lookForAt('structure', xPosition, yPosition)[0];
+        var energyStoredInLink = link.store[RESOURCE_ENERGY];
+        if (energyStoredInLink > neededValue) {
+            return link
+        } else {
+            return "empty"
+        }
+
+    }
